c92ffa8659450e708e9460615f7691427e57c7ca
Yulia-Z <yzavarik@gmail.com>
2020-03-09 00:17:19 -0700

    added CounterItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import CounterItem from "./CounterItem";
 
 function App() {
   return (
     <div className="App">
-
+      <CounterItem/>
     </div>
   );
 }
---
+++ src/CounterItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function CounterItem() {
+  return (
+    <div>
+      Counter name
+      <button>-</button>
+      Value
+      <button>+</button>
+      <button>DELETE</button>
+    </div>
+  );
+}
+
+export default CounterItem;
